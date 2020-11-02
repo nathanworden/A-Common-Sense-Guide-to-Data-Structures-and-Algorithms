c3db794959d6f689364108ce0f1085e625b1376b
Nathan Worden <worden.nathan@gmail.com>
2020-11-02 11:39:40 -0800

    hash tables, stacks and queues

--- 11.chap8page215_hash_table.rb
+++ 11.chap8page215_hash_table.rb
@@ -18,7 +18,98 @@
 # On classic approach for handling collisions is known as separate chaining. When a collision occurs, instead of placing a single value in the cell, it places in it a reference to an array.
 
 # Ultimately, a hash table's efficiency depends on three factors:
-#  - How much data we're storing in the hash table
+# - How much data we're storing in the hash table
 # - How many cells are available in the hash table
 # - Which hash function we're using
 
+# A good hash function is one that distributes its data across all available cells.
+
+# A good hash table strikes a balance of avoiding collisions while not consuming lots of memory.
+
+# Hash table rule of thumb: for every seven data elements stored in a hash table, it should have ten cells.
+
+# This ratio of data to cells is called the load factor. Using this terminology, we'd say that the ideal load factor is .7.
+
+
+
+# Exercise:
+# Write a function that returns the intersection of two arrays. The intersection is a third array that contains all values contained within the first two arrays. For example, the intersection of [1, 2, 3, 4, 5] and [0, 2, 4, 6, 8] is [2, 4]. Your function should have a complexity of O(N).
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [0, 2, 4, 6, 8]
+
+# def intersection(arr1, arr2)
+#   output = []
+
+#   arr2.each do |element|
+#     output.push(element) if arr1.include?(element)
+#   end
+
+#   output
+# end
+
+# p intersection(arr1, arr2)
+
+def intersection(arr1, arr2)
+  hsh = {}
+  output = []
+  arr1.each do |ele|
+    hsh[ele] = true
+  end
+
+  arr2.each do |ele|
+    output.push(ele) if hsh[ele] 
+  end
+
+  output
+end
+
+# p intersection(arr1, arr2)
+
+
+#### 2
+
+def duplicate(arr)
+  hsh = {}
+
+  arr.each do |ele|
+    return ele if hsh[ele]
+    hsh[ele] = true
+  end
+
+  false
+end
+
+dup = ["a", "b", "c", "d", "c", "e", "f"]
+
+# p duplicate(dup)
+
+
+### 3
+
+str = "the quick brown box jumps over a lazy dog"
+def find(str)
+  hsh = {}
+  str.gsub(" ", "").split('').each {|ele| hsh[ele] = true}
+  ("a".."z").to_a.map {|ele| return ele if !hsh[ele]}
+end
+
+p find(str)
+
+
+### 4
+
+def non_dup(str)
+  hsh = {}
+  str.split('').each do |ele| 
+    if hsh[ele]
+      hsh[ele] += 1
+    else
+      hsh[ele] = 1
+    end
+  end
+
+  hsh.each {|key, value| return key if value == 1}
+end
+
+p non_dup("minimum")
---
+++ 12.chap9.page241_stacks_and_queues.rb
@@ -0,0 +1,28 @@
+# Stacks and Queues
+
+# Stacks and Queues are simply arrays with restrictions. Yet, these restrictions are what makes them so elegant.
+
+# Stacks and queues are elegant tools for handling temporary data.
+
+# Stacks and Queues serve as temporary containers that can be used to form beautifuyl algoritms.
+
+# Temporary data is information that doesn't have any meaning after it's procesed, so you can throw it away once you're done with it.
+
+# Stacks and queues have a special focus on the order in which the data is handled.
+
+########
+# Stacks
+
+# A stack stores data in the same way that arrays do- it's simply a list of elements. The one catch is that stacks have the folowing three constraints:
+
+# 1) Data can be inserted only at the end of a stack.
+# 2) Data can be deleted only from the end of the stack
+# 3) Only the last element of a stack can be read.
+
+# You an think of a stack as an actual stack of dishes: you can't look at the face of any dish other than the one at the top. Simililarly, you can't add any dish except to the top of the stack, nor can you remove any dish beshides the one at the top.
+
+
+########
+
+
+
