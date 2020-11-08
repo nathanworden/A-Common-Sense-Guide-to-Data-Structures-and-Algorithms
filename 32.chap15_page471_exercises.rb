49b5aa08d4ed00757adb94a292b2c8aa651592f9
Nathan Worden <worden.nathan@gmail.com>
2020-11-08 10:07:45 -0800

    heaps

---
+++ 32.chap15_page471_exercises.rb
@@ -0,0 +1,30 @@
+                    # 1
+                    #     5
+                    # 2      9
+                    #   4   6   10
+                    # 3       8
+
+
+# def greatest(node)
+#   current_node = node
+#   while node.rightChild
+#     curremt_node = node.rightChild
+#   end
+#   current_node
+# end
+
+# Moby  Dick
+# Great Expectations
+# Alice in Wonderland
+# Lrd of the Flies
+# Robinson Crusoe
+# Pride and Prejudice
+# The Odyssey
+
+Alice in Wonderland
+Lord of the Flies
+Great Expectations
+Pride and Prejudice
+The Odyssey
+Robinson Crusoe
+Moby Dick
---
+++ 33.chap16_page_473_heaps.rb
@@ -0,0 +1,28 @@
+# Heaps
+
+# A heap is a type of tree data structure that is ueseful when we want to constantly keep tabs on the greatest or least data element in a dataset
+
+# Different data structure: Priority Queue
+
+# The queue is a list in which items are processed First In, First Out.
+
+# A priority queue is a list whose deletions and access are just like a classic queue, but whose insertions are like an ordered array.
+
+# One way we can implement a priority queue is by using a simple ordered array. That is, we use an array, and aply the following constraints:
+# - When we insert data, we always ensure that we maintain proper order.
+# - Data can only be deleted from the end of the array
+
+# The priorty queue has two primary operations: deletions and insertions.
+
+
+##### HEAPS  ###
+
+# There are several different types of heaps, but we're going to focus on the binary heap.
+
+# The binary heap is a specific kind of binary tree.
+
+# The binary max-heap is a binary tree that maintains the following conditions:
+
+# - The value of each node must be greater than each of its descendant nodes. This condition is known as the heap condition.
+# - The tree must be complete
+
