e56f86479689d477d217c93105efda72bdcedcc3
Nathan Worden <worden.nathan@gmail.com>
2020-11-14 19:28:19 -0800

    quicksort

---
+++ 25.quicksort.rb
@@ -0,0 +1,9 @@
+# Quicksort is an extremely fast sorting algorithm that is particularly efficient for average scenarios. While in worst case scenarios (that is, invesrsly sorted arrays), it performs similarly to Insertion Sort and Selection Sort, it is much faster for average scenarios- which are what occur most of the time.
+
+# Quicksort relies on a concept called partitioning.
+
+# To partition an array is to take a random value from the array, which is called the pivot, and make sure that every number that is less than the pivot ends up to the left of the pivot, and every number that is greater than the pivot ends up to the right of the pivot.
+
+# Paritioning:
+
+# Let's say we have the following array:
